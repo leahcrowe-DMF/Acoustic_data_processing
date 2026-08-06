786d961ad8d19320e276102736825c41ff663851
leahcrowe-DMF <leah.m.crowe@mass.gov>
2026-08-06 16:18:38 +0000

    add noise floor output

--- script/QAQCreport.R
+++ script/QAQCreport.R
@@ -1,14 +1,16 @@
 # libraries ----
 
-library(PAMmisc);library(PAMscapes);library(dplyr)
+library(PAMmisc);library(PAMscapes);library(dplyr);library(lubridate)
 
 # input variables ----
 
 drive_folder = "P:/"
 #drive_folder = "D:/DMF_PAM/"
-site = "MBW05"
-deployment_number = "03"
-ST_ID = "8827"
+site = "TIL15"
+deployment_number = "02"
+ST_ID = "8855"
+
+# path to data ----
 
 path<-paste0(drive_folder,'/',site,'/',site,'_',deployment_number,'/',ST_ID)
 
@@ -31,4 +33,23 @@ qaqcData <- evaluateDeployment(dir=path, sensitivity=-172.5, excludeDirs = "Init
                                outDir=paste0(path,"/QAQC_Output"))
 
 
-runQAQCReview(data = paste0(path,"/QAQC_Output/",ST_ID,"_QAQCData.csv"))
+runQAQCReview(data = paste0(path,"/QAQC_Output/",site,"_",deployment_number,"-",ST_ID,"_QAQCData.csv"))
+
+# output for noise floor investigation ----
+
+TOL<-read.csv(paste0(path, "/QAQC_Output/",site,"_",deployment_number,"-",ST_ID,"_QAQCData.csv"), header = T)
+
+noise_floor<-TOL%>%filter(TOL_16000 > TOL_500)%>%
+  mutate(TOL_16000_dif = lead(TOL_16000) - TOL_16000)%>%
+  dplyr::select(TOL_16000, TOL_16000_dif, UTC)%>%
+  mutate(UTC = ymd_hms(UTC, tz = "UTC"))%>%
+  mutate(datetime_ET = with_tz(UTC, tz = "America/New_York"))%>%
+  group_by(date = as.Date(datetime_ET))%>%
+  mutate(num_hours = n(),
+         min_hour_ET = min(datetime_ET),
+         max_hour_ET = max(datetime_ET))%>%
+  distinct(min_hour_ET, max_hour_ET, num_hours)%>%
+  mutate(Comments = "")%>%
+  filter(!is.na(date))
+
+write.csv(noise_floor, paste0(path,"/QAQC_Output/",site,"_",deployment_number,"-",ST_ID,"_noisefloor.csv"), row.names = F)
